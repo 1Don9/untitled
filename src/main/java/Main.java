fa2b649c5f7d68068cbf46bff6bddbd8af8a5098
五月花开 <120485939+1Don9@users.noreply.github.com>
2023-07-09 15:15:17 +0800

    Update Main.java
    t1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,6 +3,7 @@ import org.w3c.dom.ls.LSOutput;
 public class Main {
     public static void main(String[] args) {
         System.out.println("1111111");
-        System.out.println("2222222222");
+        System.out.println("2222222222111111111");
+        
     }
 }
