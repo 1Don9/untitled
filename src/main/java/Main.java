c28223eaaa7b72d7a022c058112786230c050868
1Don9 <wyhk025@gmail.com>
2023-07-09 15:43:39 +0800

    1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,8 +2,8 @@ import org.w3c.dom.ls.LSOutput;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("1111111");
+        System.out.println("1111111222222222222");
         System.out.println("2222222222111111111");
-        
+
     }
 }
