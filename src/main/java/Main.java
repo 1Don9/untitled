8d7fdf10cf6036903a2585a100d6e6a43fe294e7
1Don9 <wyhk025@gmail.com>
2023-07-09 15:49:24 +0800

    121323

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,6 +4,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("1111111222222222222");
         System.out.println("2222222222111111111");
+        System.out.println('2');
+        System.out.println(213434525);
 
     }
 }
